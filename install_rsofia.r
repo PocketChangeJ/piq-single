ff9be86a83a0407b7ac0a232c6d657f27a6d09eb
PocketChangeJ <21047796+PocketChangeJ@users.noreply.github.com>
2020-01-09 16:31:54 -0800

    Create install_rsofia.r

---
+++ install_rsofia.r
@@ -0,0 +1,11 @@
+---
+title: "install rsofia"
+output: html_notebook
+---
+
+
+
+```{r}
+package.url <- "https://cran.r-project.org/src/contrib/Archive/RSofia/RSofia_1.1.tar.gz"
+install.packages(package.url, repos=NULL, type="source")
+```
